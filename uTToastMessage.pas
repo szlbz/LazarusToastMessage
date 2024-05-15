8b697ee4234e71249b2561ebf31b745a2fb85a9f
szlbz <109887617+szlbz@users.noreply.github.com>
2024-05-15 11:09:38 +0800

    Add files via upload

--- uTToastMessage.pas
+++ uTToastMessage.pas
@@ -1,5 +1,12 @@
 unit uTToastMessage;
 {------------------------------------------------------------------------------
+秋风(QQ:315795176)
+  来自https://github.com/pcplayer/DelphiToastMessage
+  基于pcplayer版本，修改后适用于lazarus,并可以跨平台使用
+1、根据显示内容调整totast的宽度，防止显示内容不完整。
+2、增加从form底向上显示选择
+秋风 2024-05-12
+
   来自 https://github.com/desenvolvimentojd3/DelphiToastMessage
 
   我对它进行了一些修改：
